7343abcf1a6b012cf43561affada9cb44839881f
Marcelo Jhones <marceloj@acad.ifma.edu.br>
2023-07-17 20:17:46 -0300

    mais componentes

--- src/App.js
+++ src/App.js
@@ -1,20 +1,34 @@
 import logo from './logo.svg';
 import './App.css';
 
+
+const Saudacao = (props)=>{
+  return (
+    <h2>
+      <span> Olá</span>
+      <strong> Mundo2</strong>
+      <p>{props.name}, { props.idade}</p>
+      !!!
+    </h2>
+  )
+}
+
+
+function MeuBotao(){
+  const label = "clique aqui 2"
+  return (
+    <button type='button'>{label}</button>
+  )
+}
 function App() {
   return (
     <div className="App">
-     <h1>
-        <span> Olá</span>
-        <strong> Mundo</strong>
-        !!!
-     </h1>
-     <article>
-      <h2>Subtitulo</h2>
-      <p>OAosklas</p>
-     </article>
-
-     <button type='button'>Clique aqui!!</button>
+      <Saudacao name = "Marcelo" idade = "30"/>
+      <article>
+        <h2>Subtitulo</h2>
+        <p>OAosklas</p>
+      </article>
+      <MeuBotao/>
     </div>
   );
 }
