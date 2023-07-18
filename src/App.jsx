adefc13f16e37cdc79ab3d326f48d206a3ee596c
MarceloJhones1 <8605858-MarceloJhones1@users.noreply.replit.com>
2023-07-18 17:57:22 +0000

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,79 @@
+
+import Voos from './Voos';
+
+const Saudacao = (props)=>{
+  return (
+    <h2>
+      <span> Olá</span>
+      <strong> Mundo2</strong>
+      <p>{props.name}, { props.idade}</p>
+      !!!
+    </h2>
+  )
+}
+
+
+function MeuBotao(props){
+  const label = "clique aqui 2"
+  return (
+    <button type='button'>{label}</button>,<button type='submit'>{props.label}</button>
+
+  )
+}
+
+
+function PessoaIdade(props){
+  return (
+    <strong>{props.label}</strong>
+  )
+}
+function Pessoa(props){
+  return (
+    <section>
+      Você é {props.idade>=18? <PessoaIdade label="de maior"/>: <PessoaIdade label =  "de menor"/>}
+    </section>
+  )
+}
+const ListItem = (props) => {
+  return(
+    <li><strong>{props.label}</strong></li>
+  )
+}
+
+const  Lista = ()=>{
+
+  const passos = [
+    "configurar projeto",
+    "Embedar react",
+    "Criar componentes",
+    "Escrever testes"
+  ]
+  return(
+    <ul>
+      {passos.map((valor,index)=>{
+        return  <ListItem key={`item-${index}`}  label ={valor}/>
+      })}
+    </ul>
+  )
+}
+
+export default function App() {
+
+  const labelBtn = `clique aqui ${2}x`
+  return (
+    <div className="App">
+      <Saudacao name = "Marcelo" idade = "25"/>
+      <Voos/>
+      <Lista/>
+      <Pessoa idade={19}/>
+      <Pessoa idade={16}/>
+      
+      <article>
+        <h2>Subtitulo</h2>
+        <p>OAosklas</p>
+      </article>
+      <MeuBotao label = {labelBtn} />
+    </div>
+  );
+}
+
---
+++ src/Voos.jsx
@@ -0,0 +1,79 @@
+
+const voosDisponíveis = [
+    {
+        id:'123',
+        title: "Volta ter. 19 de Julho",
+        details:[
+            {
+            id: '132',
+            title:"Espaço para pernas"
+        },
+        {
+            id: '133',
+            title:"Vídeo sob demanda"
+        },
+        {
+            id: '134',
+            title:"Saída usb no assento"
+        },
+        {
+            id: '135',
+            title:"Estima das emissoes de Carbono: 83kg"
+        }]
+    },
+    {
+        id:'151',
+        title: "13:00 - 15:00",
+        details:[
+            {
+            id: '132',
+            title:"Espaço para pernas"
+        },
+        {
+            id: '133',
+            title:"Vídeo sob demanda"
+        },
+        {
+            id: '134',
+            title:"Saída usb no assento"
+        },
+        {
+            id: '135',
+            title:"Estima das emissoes de Carbono: 83kg"
+        }]
+    }
+]
+function ItemVooDetails({ details }) {
+  return (
+    <ul>
+      {details.map(detail => {
+        return <li key={detail.id}>{detail.title}</li>;
+      })}
+    </ul>
+  );
+}
+
+function ItemVoo(props) {
+  return (
+    <div className="voo">
+      <header>
+        <h3>{props.title}</h3>
+      </header>
+      <div className="voo-details">
+        <ItemVooDetails details={props.details} />
+      </div>
+    </div>
+  );
+}
+
+export default function Voos() {
+  return (
+    <div className="voos">
+      {voosDisponíveis.map(voo => {
+        return <ItemVoo key={voo.id} title={voo.title} details={voo.details} />;
+      })}
+    </div>
+  );
+}
+
+
